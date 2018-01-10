c1afbf8b88b9e6c54a3312404b2f988039f8dcdf
KDekho01 <ksenia.dekhonova@isobar.ru>
2018-01-10 19:10:06 +0300

    initial commit

---
+++ src/components/StartScreen.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="alert alert-secondary">
+        <h3>Are you ready?</h3>
+        <button class="btn btn-success" @click="$emit('onStart')">Yes!</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+        	some() {
+        		alert(1)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .alert {
+        text-align: center;
+    }
+    h3, .btn {
+        margin: 20px 0;
+    }
+</style>
